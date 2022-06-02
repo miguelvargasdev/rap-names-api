daa366ca94a7d51dda6742a317c0cead10f407b3
Miguel Vargas <mavargas@gmail.com>
2022-06-01 20:36:58 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+const rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, UK'
+    },
+    'chance the rapper': {
+        'age': 29,
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois'
+    } 
+}
+
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/api/:rapperName', (request, response) => {
+   const rappersName = request.params.rapperName.toLowerCase();
+    if(rappers[rappersName]){
+        response.json(rappers[rappersName]);
+    }else{
+        response.json(rappers['dylan']);
+    }
+})
+app.listen(PORT, ()=> {
+    console.log(`The server is running on port ${PORT}!`);
+});
